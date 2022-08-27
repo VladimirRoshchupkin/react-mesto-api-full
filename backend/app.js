20d20f033343f89a21668aaebbf73e1541d58fc5
Vladimir Roshchupkin <vladimir.roschupkin@rusvinyl.ru>
2022-08-27 21:08:41 +0300

    PR15 121

--- backend/app.js
+++ backend/app.js
@@ -19,8 +19,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 // mongoose.connect('mongodb://127.0.0.1:27017/mestodb', { family: 4 });
 const { PORT = 3000 } = process.env;
 const app = express();
-app.use(cookieParser());
 app.use(corsRules);
+app.use(cookieParser());
 
 app.use(helmet());
 app.use(express.json());
